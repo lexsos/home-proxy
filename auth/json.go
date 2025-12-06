7d6937c3831b2569cfe37a2b2f80d9f3b68164e0
Alexander Sosnin <alesosnin@yandex-team.ru>
2025-12-06 20:01:03 +0300

    Add constructor for json auth

--- auth/json.go
+++ auth/json.go
@@ -1,7 +1,10 @@
 package auth
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"os"
 )
 
 type JsonAccount struct {
@@ -15,8 +18,22 @@ type JsonHttpAuthenticator struct {
 	accounts map[string]JsonAccount
 }
 
-func NewJsonHttpAuthenticator(FileName string) (*JsonAccount, error) {
-	
+func NewJsonHttpAuthenticator(fileName string) (*JsonHttpAuthenticator, error) {
+	data, err := os.ReadFile(fileName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file %s: %w", fileName, err)
+	}
+	var accounts []JsonAccount
+	if err := json.Unmarshal(data, &accounts); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal JSON: %w", err)
+	}
+	accountsMap := make(map[string]JsonAccount, len(accounts))
+	for _, account := range accounts {
+		accountsMap[account.Login] = account
+	}
+	return &JsonHttpAuthenticator{
+		accounts: accountsMap,
+	}, nil
 }
 
 func (jsonAuth *JsonHttpAuthenticator) GetUser(r *http.Request) (*Account, error) {
@@ -26,6 +43,9 @@ func (jsonAuth *JsonHttpAuthenticator) GetUser(r *http.Request) (*Account, error
 	}
 	account, ok := jsonAuth.accounts[lp.Login]
 	if !ok {
+		return nil, nil
+	}
+	if account.Password != lp.Password {
 		return nil, nil
 	}
 	return &Account{
