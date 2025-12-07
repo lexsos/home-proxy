a2358fbd545158398d4019df2b30eb3f5e0da304
Alexander Sosnin <alesosnin@yandex-team.ru>
2025-12-07 20:52:37 +0300

    Add function for parse domain

--- domains/json/matcher.go
+++ domains/json/matcher.go
@@ -1,5 +1,28 @@
 package json
 
+import "strings"
+
+type parsedDomain struct {
+	original string
+	parents  []string
+}
+
+func prepareDomain(domain string) *parsedDomain {
+	original := strings.ToLower(domain)
+
+	var parents []string
+	parts := strings.Split(original, ".")
+	for i := 1; i < len(parts); i++ {
+		parent := strings.Join(parts[i:], ".")
+		parents = append(parents, parent)
+	}
+
+	return &parsedDomain{
+		original: original,
+		parents:  parents,
+	}
+}
+
 func (repo *DomainSetRepository) Match(domain string, sets []string) bool {
 	return false
 }
