6952715e866e8887df7fa57f8f2f2aa73218ebef
Alexander Sosnin <alesosnin@yandex-team.ru>
2025-12-07 20:36:11 +0300

    Add domains set constructor

--- domains/json/constructor.go
+++ domains/json/constructor.go
@@ -1,5 +1,11 @@
 package json
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
 type jsonDomain struct {
 	Dns  string    `json:"dns"`
 	Type MatchType `json:"type"`
@@ -7,7 +13,7 @@ type jsonDomain struct {
 
 type jsonDomainSet struct {
 	Slug    string       `json:"slug"`
-	Domains []jsonDomain `json:"ips"`
+	Domains []jsonDomain `json:"domains"`
 }
 
 type jsonConfig struct {
@@ -15,5 +21,38 @@ type jsonConfig struct {
 }
 
 func NewDomainSetRepository(fileName string) (*DomainSetRepository, error) {
-	return &DomainSetRepository{}, nil
+	// Read the file
+	data, err := os.ReadFile(fileName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read config file: %w", err)
+	}
+
+	// Parse JSON
+	var config jsonConfig
+	if err := json.Unmarshal(data, &config); err != nil {
+		return nil, fmt.Errorf("failed to parse config file: %w", err)
+	}
+
+	// Build the repository
+	domainsSets := make(map[string]DomainsSet)
+
+	for _, domainSet := range config.DomainsSets {
+		domainsMap := make(map[string]Domain)
+
+		for _, domain := range domainSet.Domains {
+			domainsMap[domain.Dns] = Domain{
+				Dns:  domain.Dns,
+				Type: domain.Type,
+			}
+		}
+
+		domainsSets[domainSet.Slug] = DomainsSet{
+			Slug:    domainSet.Slug,
+			Domains: domainsMap,
+		}
+	}
+
+	return &DomainSetRepository{
+		domainsSets: domainsSets,
+	}, nil
 }
