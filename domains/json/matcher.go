15bc9d9dfbbd4ce591ed921a21e1775a35372813
Alexander Sosnin <alesosnin@yandex-team.ru>
2025-12-08 20:11:54 +0300

    Add match function for domains

--- domains/json/matcher.go
+++ domains/json/matcher.go
@@ -24,5 +24,28 @@ func prepareDomain(domain string) *parsedDomain {
 }
 
 func (repo *DomainSetRepository) Match(domain string, sets []string) (bool, error) {
+	data := prepareDomain(domain)
+	for _, setSlug := range sets {
+		// Get domain set by slug, if not found continue loop
+		domainSet, ok := repo.domainsSets[setSlug]
+		if !ok {
+			continue
+		}
+		// Try check original DNS for domain in current domains set
+		domain, ok := domainSet.Domains[data.original]
+		if ok && (domain.Type == Exact || domain.Type == SubDomains) {
+			return true, nil
+		}
+		// Try check parent domains in current domains set
+		for _, parent := range data.parents {
+			domain, ok := domainSet.Domains[parent]
+			if !ok {
+				continue
+			}
+			if domain.Type == SubDomains {
+				return true, nil
+			}
+		}
+	}
 	return false, nil
 }
