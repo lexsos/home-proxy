241f8cdc265ff060f19feb9501b0797fef052ec6
Alexander Sosnin <alesosnin@yandex-team.ru>
2025-12-10 21:23:17 +0300

    Add method for extract profile config

--- profiles/inmemory/finder.go
+++ profiles/inmemory/finder.go
@@ -1,7 +1,24 @@
 package inmemory
 
-import "github.com/lexsos/home-proxy/profiles"
+import (
+	"fmt"
+
+	"github.com/lexsos/home-proxy/profiles"
+)
 
 func (repo *InMemoryProfilesRepository) GetProfile(slug string) (*profiles.ProfileConfig, error) {
-	return nil, nil
+	profile, ok := repo.profiles[slug]
+	if !ok {
+		return nil, fmt.Errorf("Profile '%s' not found", slug)
+	}
+	currentTime := profiles.NowTimeInLocation(profile.tz)
+	for _, timeRange := range profile.timeRanges {
+		if (currentTime >= timeRange.startAt) && (currentTime <= timeRange.endAt) {
+			return &profiles.ProfileConfig{
+				Policy:      timeRange.policy,
+				DomainsSets: timeRange.domainsSets,
+			}, nil
+		}
+	}
+	return nil, fmt.Errorf("Time range not found in profile '%s'", slug)
 }
