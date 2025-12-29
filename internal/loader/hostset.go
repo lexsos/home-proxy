91ff2d8875a7aeb2935c7ea601e9d50c3837d495
Alexander Sosnin <alesosnin@yandex-team.ru>
2025-12-29 18:18:28 +0300

    Add host repository loader

---
+++ internal/loader/hostset.go
@@ -0,0 +1,64 @@
+package loader
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/lexsos/home-proxy/internal/hostset"
+	"github.com/lexsos/home-proxy/internal/hostset/domainset"
+	"github.com/lexsos/home-proxy/internal/hostset/ipset"
+)
+
+type MatchType string
+
+const (
+	ExactHost       MatchType = "exact"
+	SubDomainsHosts MatchType = "subdomains"
+	Ip              MatchType = "ip"
+)
+
+type jsonHost struct {
+	Host string    `json:"host"`
+	Type MatchType `json:"type"`
+}
+
+type jsonHostSet struct {
+	Slug  string     `json:"slug"`
+	Hosts []jsonHost `json:"hosts"`
+}
+
+type jsonHostSets struct {
+	HostSets []jsonHostSet `json:"hosts_sets"`
+}
+
+func LoadHostRepository(fileName string) (hostset.HostRepository, error) {
+	data, err := os.ReadFile(fileName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read config file: %w", err)
+	}
+	var hostSets jsonHostSets
+	if err := json.Unmarshal(data, &hostSets); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal config file: %w", err)
+	}
+	repo := hostset.NewInMemoryHostRepository()
+	for _, hostSet := range hostSets.HostSets {
+		ips := ipset.NewInMemoryIpSet()
+		domains := domainset.NewInMemoryDomainsSet()
+		for _, host := range hostSet.Hosts {
+			switch host.Type {
+			case ExactHost:
+				domains.Add(host.Host, domainset.ExactDomain)
+			case SubDomainsHosts:
+				domains.Add(host.Host, domainset.SubDomains)
+			case Ip:
+				err := ips.Add(host.Host)
+				if err != nil {
+					return nil, fmt.Errorf("failed to add ip to set: %w", err)
+				}
+			}
+		}
+		repo.AddHostSet(hostSet.Slug, ips, domains)
+	}
+	return repo, nil
+}
