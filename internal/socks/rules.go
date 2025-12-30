6cb520c2e5448c925cfba0770f61ed87321d24dd
Alexander Sosnin <alesosnin@yandex-team.ru>
2025-12-30 17:18:37 +0300

    rewite socks rules

--- internal/socks/rules.go
+++ internal/socks/rules.go
@@ -5,22 +5,20 @@ import (
 
 	"github.com/armon/go-socks5"
 
+	"github.com/lexsos/home-proxy/internal/auth"
 	"github.com/lexsos/home-proxy/internal/filters"
 	"github.com/lexsos/home-proxy/internal/utils/logging"
 )
 
-var allowedMethods = map[uint8]struct{}{
-	socks5.NoAuth:       {},
-	socks5.UserPassAuth: {},
-}
-
 type SocksRules struct {
-	filter filters.RequestFilter
+	filter        filters.RequestFilter
+	authenticator auth.Authenticator
 }
 
-func NewSocksRules(filter filters.RequestFilter) *SocksRules {
+func NewSocksRules(filter filters.RequestFilter, authenticator auth.Authenticator) *SocksRules {
 	return &SocksRules{
-		filter: filter,
+		filter:        filter,
+		authenticator: authenticator,
 	}
 }
 
@@ -47,17 +45,43 @@ func (rules *SocksRules) Allow(ctx context.Context, req *socks5.Request) (contex
 
 func (rules *SocksRules) profile(ctx context.Context, req *socks5.Request) string {
 	logger := logging.LogFromContext(ctx)
-	_, hasAith := allowedMethods[req.AuthContext.Method]
-	if !hasAith {
-		logger.Debugf("invalid auth method %d for '%s'", req.AuthContext.Method, req.RemoteAddr.IP)
+	switch req.AuthContext.Method {
+	case socks5.NoAuth:
+		src := req.RemoteAddr.IP.String()
+		logger.Debugf("auth by ip '%s' in socks rules", src)
+		account, err := rules.authenticator.AuthByIp(ctx, src)
+		if err != nil {
+			logger.Errorf("fail auth by ip in socks rules: %s", err)
+			return ""
+		}
+		if account == nil {
+			logger.Debugf("unknown account for '%s'", src)
+			return ""
+		}
+		logger.Debugf("success auth by ip for '%s'", account.Login)
+		return account.ProfileSlug
+	case socks5.UserPassAuth:
+		logger.Debugf("auth by login in socks rules")
+		login, ok := req.AuthContext.Payload["Username"]
+		if !ok {
+			logger.Debugf("login not found for '%s'", req.RemoteAddr.IP)
+			return ""
+		}
+		account, err := rules.authenticator.GetByLogin(login)
+		if err != nil {
+			logger.Errorf("fail get account by login in socks rules: %s", err)
+			return ""
+		}
+		if account == nil {
+			logger.Debugf("unknown account for '%s'", login)
+			return ""
+		}
+		logger.Debugf("success auth by login for '%s'", account.Login)
+		return account.ProfileSlug
+	default:
+		logger.Debugf("unknown auth method %d for '%s'", req.AuthContext.Method, req.RemoteAddr.IP)
 		return ""
 	}
-	profile, ok := req.AuthContext.Payload[profileField]
-	if !ok {
-		logger.Debugf("profile not found for '%s'", req.RemoteAddr.IP)
-		return ""
-	}
-	return profile
 }
 
 func (rules *SocksRules) dst(req *socks5.Request) string {
